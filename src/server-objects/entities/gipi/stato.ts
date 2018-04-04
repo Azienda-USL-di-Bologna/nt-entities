046db5e5535e79f036eb30aeffc61370c68f9935
s.fayssel <devnull@localhost>
2018-04-04 12:46:19 +0200

    Aggiunti stati iter come costanti

--- src/server-objects/entities/gipi/stato.ts
+++ src/server-objects/entities/gipi/stato.ts
@@ -1,9 +1,11 @@
 import { Entity, OdataForeignKey } from "@bds/nt-context";
+import { CODICE_STATI } from "../../../client-objects/constants/stati-iter";
+import { DESCRIZIONE_STATI } from "../../../client-objects/constants/stati-iter";
 
 export class Stato extends Entity {
     public id: number;
-    public codice: string;
-    public descrizione: string;
+    public codice: CODICE_STATI;
+    public descrizione: DESCRIZIONE_STATI;
 
     getName(): string {
         return "Statos";
