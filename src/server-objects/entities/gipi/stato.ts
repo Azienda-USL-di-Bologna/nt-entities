7e439b1cb695cb9ac5bff05eaf6d5006cbc386c6
s.fayssel <devnull@localhost>
2018-04-04 16:11:39 +0200

    update stati iter

--- src/server-objects/entities/gipi/stato.ts
+++ src/server-objects/entities/gipi/stato.ts
@@ -1,11 +1,12 @@
 import { Entity, OdataForeignKey } from "@bds/nt-context";
-import { CODICE_STATI } from "../../../client-objects/constants/stati-iter";
-import { DESCRIZIONE_STATI } from "../../../client-objects/constants/stati-iter";
+import { IN_CORSO_CODICE,
+            SOSPESO_CODICE,
+            CHIUSO_CODICE} from "../../../client-objects/constants/stati-iter"
 
 export class Stato extends Entity {
     public id: number;
-    public codice: CODICE_STATI;
-    public descrizione: DESCRIZIONE_STATI;
+    public codice: string;
+    public descrizione: string;
 
     getName(): string {
         return "Statos";
