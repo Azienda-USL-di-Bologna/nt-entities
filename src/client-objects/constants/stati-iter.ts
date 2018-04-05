00b7b1f4846a3c7e426a4f9546368d4ecd62647d
s.fayssel <devnull@localhost>
2018-04-05 09:24:20 +0200

    update stati

---
+++ src/client-objects/constants/stati-iter.ts
@@ -0,0 +1,20 @@
+export const IN_CORSO_CODICE: string = "IN_CORSO";
+export const SOSPESO_CODICE: string = "SOSPESO";
+export const CHIUSO_CODICE: string = "CHIUSO";
+
+export const IN_CORSO_VALUE: string = "IN CORSO";
+export const SOSPESO_VALUE: string = "SOSPESO";
+export const CHIUSO_VALUE: string = "CHIUSO";
+
+
+// export enum CODICE_STATI{
+//     IN_CORSO = "IN_CORSO",
+//     SOSPESO = "SOSPESO",
+//     CHIUSO = "CHIUSO"
+// }
+
+// export enum DESCRIZIONE_STATI{
+//     IN_CORSO = "IN CORSO",
+//     SOSPESO = "SOSPESO",
+//     CHIUSO = "CHIUSO"
+// }
